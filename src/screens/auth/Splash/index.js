0f6379599fee63e55ea0775bd3703341bb2bc2f6
Student 2022 <student@voco.ee>
2023-09-27 13:22:01 +0300

    27.09

--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -4,15 +4,18 @@ import {
     View,
     Image
  } from "react-native"
+ import Button from "../../../components/Button";
  import { styles } from "./styles";
 
 const Splash = () => {
     return (
         <View style={styles.container}>
             <Image resizeMode="contain" style={styles.image} source={require('../../../assets/splash_image.png')}/>
-            <Text style={styles.title}>You'll Find Text</Text>
-            <Text style={[styles.title, styles.innerTitle]}> All you need</Text>
-            <Text style={styles.title}> Here!</Text>
+            <Text style={[styles.title, styles.textContainer]}>You'll Find Text</Text>
+            <Text style={[styles.title, styles.innerTitle, styles.textContainer]}> All you need</Text>
+            <Text style={[styles.title, styles.textContainer]}> Here!</Text>
+
+            <Button title="Sign up" />
         </View>
     )
 }
--- src/screens/auth/Splash/styles.js
+++ src/screens/auth/Splash/styles.js
@@ -16,7 +16,7 @@ export const styles = StyleSheet.create({
     container: {
         padding: 9
     },
-    title, innerTitle: {
+    textContainer: {
         textAlign: "center"
     }
 })
