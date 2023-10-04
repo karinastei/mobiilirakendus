38d38ca32697789142cb8c5667e2800d5d692d58
Student 2022 <student@voco.ee>
2023-10-04 13:40:21 +0300

    signup failed try

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {SafeAreaView} from 'react-native';
 import Splash from './src/screens/auth/Splash';
 import AuthHeader from './src/components/AuthHeader';
 import Signup from './src/screens/auth/Signup';
 import Input from './src/components/Input';
 
+//const REVERSED_CLIENT_ID = 'com.googleusercontent.apps.79243600821-jdm74p3nr5frlui1p9hqismip60vui8m'
+
 const App = () => {
+  //useEffect(() => {}, [])
   return (
     <SafeAreaView>
        <Signup/>
